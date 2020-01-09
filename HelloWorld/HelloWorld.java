e94d73280b9c2f2911321e9062833ce404d87e52
Seonju Jung <W0438081@nscc.ca>
2020-01-09 12:17:22 -0400

    initial version

--- HelloWorld/HelloWorld.java
+++ HelloWorld/HelloWorld.java
@@ -2,8 +2,13 @@
     class HelloWorld {
 
         public static void main(String[] args) {
+
+            int num;
+
+            num = 123456;
+
             // prints out hello
-            System.out.println("Hello World!");
+            System.out.println("num = " + num);
 
         }
 
